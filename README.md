c06f43246951df6f12eb7c31f38b3a273d679b98
alfe <alfe10251@gmail.com>
2023-03-21 15:10:08 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# print png prompt
+Print Stable Diffusion Prompt included in PNG files
+
+```sh
+$ npx printpngprompt .
+```
+
+```sh
+$ npx printpngprompt --help
+Usage: printpngprompt <dir> [options]
+
+CLI to Print Stable Diffusion Prompt included in PNG files
+
+Options:
+  -m, --markdown   show with markdown layout                 
+  -r, --recursive  recursive digging                         
+  -d, --debug      show raw PNG data                         
+  -h, --help       display help for command                  
+
+$ npx printpngprompt example.png
+sample.png
+
+1girl, computer, monitor, coding, keyboard \(computer\), desk, (Masterpiece:1.2), (best quality:1.2),
+
+Negative prompt: bad_prompt_version2, EasyNegative,  (Worst Quality, Low Quality:1.4), Normal Quality, longbody, lowres, pubic hair, extra digit, fewer digits, cropped, text, claw,
+
+- Steps: 20
+- Sampler: DPM++ 2M Karras
+- CFG scale: 7
+- Seed: 000000000
+- Size: 512x512
+- Model hash: 0000ffff00
+- Model: example-model
+- Denoising strength: 0.5
+- Clip skip: 2
+- Hires upscale: 1.4
+- Hires upscaler: R-ESRGAN 4x+ Anime6B
+
+---
+
+$ npx printpngprompt example.png --markdown
+### sample.png
+
+![img](sample.png)
+
+1girl, computer, monitor, coding, keyboard \(computer\), desk, (Masterpiece:1.2), (best quality:1.2),
+
+**Negative prompt:** bad_prompt_version2, EasyNegative,  (Worst Quality, Low Quality:1.4), Normal Quality, longbody, lowres, pubic hair, extra digit, fewer digits, cropped, text, claw,
+
+- Steps: 20
+- Sampler: DPM++ 2M Karras
+- CFG scale: 7
+- Seed: 000000000
+- Size: 512x512
+- Model hash: 0000ffff00
+- Model: example-model
+- Denoising strength: 0.5
+- Clip skip: 2
+
+---
+
+$ npx printpngprompt . -r
+
+sample1.png                                                                                                       
+
+1girl, ...
+- Steps: 20
+- Sampler: ...
+
+---                                                                                                              
+
+sample2.png                                                                                                      
+
+1girl, ...
+- Steps: 20
+- Sampler: ...
+
+---                                                                                                              
+
+dir1/sample3.png                                                                                        
+
+1girl, ...
+- Steps: 20
+- Sampler: ...
+
+---                                                                                                              
+
+```
---
+++ bin/printpngprompt.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import PNGMetadata from 'png-metadata';
+import { program } from 'commander';
+
+program
+  .name('printpngprompt')
+  .usage("<file|dir> [options]")
+  .description('CLI to Print Stable Diffusion Prompt included in PNG files')
+  .option('-m, --markdown', 'show with markdown layout')
+  .option('-r, --recursive', 'recursive digging')
+  .option('-d, --debug', 'show raw PNG data')
+
+program.parse(process.argv);
+if (program.args.length < 1) {
+  program.help();
+}
+
+const opts = program.opts();
+
+const encodeForMarkdown = (string) => {
+  return !opts.markdown ? string : string.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+}
+
+const embedStr = (str) => {
+  if (!opts.markdown) {
+    return (`\x1b[1m${str}\x1b[0m`)
+  }
+  return `**${str}**`
+}
+
+const showPngPrompt = (fileName) => {
+  if (!fileName.endsWith('.png')) return;
+
+  var data = PNGMetadata.readFileSync(fileName);
+  var list = PNGMetadata.splitChunk(data).filter((item) => item.type !== 'IDAT')
+
+  var TEXT = list.filter((item) => (
+    item.type.toUpperCase() === 'TEXT'
+    && (
+      item.data.substring(0, 10) === 'parameters'
+      // TODO: case 'Description'
+      // TODO: case 'Comment'
+    )
+  ))
+
+  const parameters = TEXT[0].data;
+  const prompt = parameters.split('Negative prompt:').shift();
+  const others = parameters.split('Negative prompt:').pop();
+  const negativePrompt = others.split('\nSteps:')[0]
+
+  const otherParams = (`Steps:${others.split('\nSteps:')[1]}`.split(/,/g))
+
+  console.log(`
+${opts.markdown ? '### ' :''}${fileName.replace(/^\.\//, '')}
+${opts.markdown ? `
+![img](${fileName})
+` :''}
+${encodeForMarkdown(prompt.substring(11).trim())}
+
+${embedStr('Negative prompt:')} ${encodeForMarkdown(negativePrompt.trim())}
+${otherParams.map((value) => {
+  if (!value.includes(':')) return (`,${value}`)
+  return (`\n- ${value.trim()}`)
+}).join('')}
+${!opts.debug ? '' : `
+#### row tEXt
+
+${parameters}
+`}
+---`)
+}
+
+const diggingPNG = (fileName) => {
+  if (fileName.endsWith('.png')) {
+    showPngPrompt(fileName);
+    return;
+  }
+  const dir = fileName;
+  fs.readdir(dir, { withFileTypes: true }, (err, files = []) => {
+    files.forEach(file => {
+      if (file.isDirectory()) {
+        if (opts.recursive) {
+          diggingPNG(`${dir}/${file.name}`)
+        }
+      } else {
+        if (file.name.endsWith('.png')) {
+          showPngPrompt(`${dir}/${file.name}`)
+        }
+      }
+    })
+  })
+}
+
+program.args.forEach(fileName => diggingPNG(fileName));
